a65e2238f79cc820d75876f9145eab4975da9bd3
mmdzov <mzov939@gmail.com>
2020-10-18 03:07:42 +0330

    updated by mmdzov

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React,{} from 'react';
+import { Route, Router, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-    
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/login'>
+          hello
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
