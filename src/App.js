1a6cda201327cf1ceac0f3fba9c841a5b115533b
mmdzov <mzov939@gmail.com>
2020-10-18 03:43:42 +0330

    add and config directories

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React,{} from 'react';
-import { Route, Router, Switch } from 'react-router-dom';
-import './App.css';
-
+import {BrowserRouter as  Router, Route, Switch,Redirect } from 'react-router-dom';
+import Home from './containers/Home/Home'
+import Login from './containers/Login/Login'
+import SignUp from './containers/SignUp/SignUp'
+import NotFound from './containers/404/404'
 function App() {
   return (
-    <Router>
+    <Router basename='/'>
       <Switch>
-        <Route path='/login'>
-          hello
-        </Route>
+        <Route path='/' exact component={Home} />
+        <Route path='/login' exact component={Login} />
+        <Route path='/signup' exact component={SignUp} />
+        <Route path='/404' exact component={NotFound} />
+        <Redirect to='/404' />
       </Switch>
     </Router>
   );
---
+++ src/containers/SignUp/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./SignUp.css"
+
+const SignUp = () => {
+    return (
+        <div className="">
+            signUp page
+        </div>
+    )
+}
+
+export default SignUp;
